523460b52abd0f3c448d34f7e6d08c7f7e9ab344
radius <ysjleader@icloud.com>
2026-02-28 17:24:48 +0900

    init

---
+++ src/main/java/com/devy/trafficstudypart3/TrafficStudyPart3Application.java
@@ -0,0 +1,13 @@
+package com.devy.trafficstudypart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficStudyPart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficStudyPart3Application.class, args);
+    }
+
+}
